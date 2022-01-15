596947cde4a3f917a04a6c07f4d803cfe4f024f5
hoontae24 <kadr8941@gmail.com>
2022-01-15 15:00:28 +0900

    Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
+  import MyComponent from "./components/MyComponent.svelte";
 </script>
 
 <h1>Hello Svelte</h1>
+<hr />
+
+<!-- Component -->
+<MyComponent />
 <hr />
---
+++ src/components/MyComponent.svelte
@@ -0,0 +1,12 @@
+<!-- MyComponent.svelte -->
+<script>
+  const msg = "world";
+</script>
+
+<h1>message: {msg}!</h1>
+
+<style>
+  h1 {
+    color: blue;
+  }
+</style>
