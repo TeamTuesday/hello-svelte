71c42bd692b32276aa999b209fc8ca4a2526e0fa
hoontae24 <kadr8941@gmail.com>
2022-01-15 15:01:50 +0900

    Props and Event

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,12 @@
 <script>
   import MyComponent from "./components/MyComponent.svelte";
   import Counter from "./components/Counter.svelte";
+  import InputForm from "./components/InputForm.svelte";
+
+  let name = "name";
+  const onNameChange = (e) => {
+    name = e.detail;
+  };
 </script>
 
 <h1>Hello Svelte</h1>
@@ -12,4 +18,10 @@
 
 <!-- Reactivity Variables(local state) -->
 <Counter />
+<hr />
+
+<!-- Reactivity Properties(props) -->
+name: {name}
+<InputForm {name} on:change={onNameChange} />
+<!-- <InputForm name={name} on:change={onNameChange} /> -->
 <hr />
---
+++ src/components/InputForm.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let name = 0;
+  const onChange = (e) => dispatch("change", e.target.value);
+</script>
+
+<div>
+  <input bind:value={name} on:input={onChange} />
+</div>
